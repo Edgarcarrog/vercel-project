f7e7d9a122dff3d8e1fd5050f45fa58f6bbfe41d
Edgarcarrog <56626783+Edgarcarrog@users.noreply.github.com>
2023-07-25 05:09:28 -0600

    axios functions

--- client/src/App.js
+++ client/src/App.js
@@ -5,10 +5,42 @@ function App() {
   return (
     <div className="App">
       <h1>Vercel Project 1.1</h1>
-      <button onClick={console.log("get")}>Get</button>
-      <button>Post</button>
-      <button>Put</button>
-      <button>Delete</button>
+      <button
+        onClick={async () => {
+          const response = await clienteAxios.get("/");
+          console.log("Get");
+          console.log(response);
+        }}
+      >
+        Get
+      </button>
+      <button
+        onClick={async () => {
+          const response = await clienteAxios.post("/");
+          console.log("Post");
+          console.log(response);
+        }}
+      >
+        Post
+      </button>
+      <button
+        onClick={async () => {
+          const response = await clienteAxios.post("/");
+          console.log("Put");
+          console.log(response);
+        }}
+      >
+        Put
+      </button>
+      <button
+        onClick={async () => {
+          const response = await clienteAxios.post("/");
+          console.log("Delete");
+          console.log(response);
+        }}
+      >
+        Delete
+      </button>
     </div>
   );
 }
