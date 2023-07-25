47a9186066d7aaac26c40c0943632a46cf681cae
Edgarcarrog <56626783+Edgarcarrog@users.noreply.github.com>
2023-07-25 05:00:10 -0600

    buttons added

--- client/src/App.js
+++ client/src/App.js
@@ -5,10 +5,10 @@ function App() {
   return (
     <div className="App">
       <h1>Vercel Project 1.1</h1>
-      <Button>Get</Button>
-      <Button>Post</Button>
-      <Button>Put</Button>
-      <Button>Delete</Button>
+      <buton>Get</buton>
+      <buton>Post</buton>
+      <buton>Put</buton>
+      <buton>Delete</buton>
     </div>
   );
 }
