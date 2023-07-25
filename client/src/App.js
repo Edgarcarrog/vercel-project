5d3531c84c1080d950853fa97ec224cd5d2285a9
Edgarcarrog <56626783+Edgarcarrog@users.noreply.github.com>
2023-07-25 04:57:17 -0600

    axios setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
-
+import { Button } from "bootstrap";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <h1>Vercel Project 1.1</h1>
+      <Button>Get</Button>
+      <Button>Post</Button>
+      <Button>Put</Button>
+      <Button>Delete</Button>
     </div>
   );
 }
